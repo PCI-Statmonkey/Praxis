2be458cc72bf24821229988743c408e9f845ad66
PCI-Statmonkey <PCI.statmonkey@gmail.com>
2026-02-12 14:51:15 -0500

    stage12: add preload ipc bridge allowlist

--- praxis-desktop/electron/preload.ts
+++ praxis-desktop/electron/preload.ts
@@ -1,24 +1,46 @@
-import { ipcRenderer, contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
+import {
+  BACKUP_GET_INVENTORY_PREVIEW,
+  PERSISTENCE_GET_DB_INTEGRITY,
+  PERSISTENCE_GET_DB_STATUS,
+  PERSISTENCE_GET_PATHS,
+  RESTORE_GET_PLAN_PREVIEW,
+  RUNTIME_GET_STATUS,
+  RUNTIME_PING,
+  SYNC_GET_STATUS,
+} from '../shared/ipc/runtimeChannels'
 
-// --------- Expose some API to the Renderer process ---------
-contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
-  },
-  off(...args: Parameters<typeof ipcRenderer.off>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.off(channel, ...omit)
-  },
-  send(...args: Parameters<typeof ipcRenderer.send>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.send(channel, ...omit)
-  },
-  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.invoke(channel, ...omit)
-  },
+export const ALLOWED_IPC_CHANNELS = [
+  RUNTIME_PING,
+  RUNTIME_GET_STATUS,
+  PERSISTENCE_GET_PATHS,
+  PERSISTENCE_GET_DB_STATUS,
+  PERSISTENCE_GET_DB_INTEGRITY,
+  SYNC_GET_STATUS,
+  BACKUP_GET_INVENTORY_PREVIEW,
+  RESTORE_GET_PLAN_PREVIEW,
+] as const
 
-  // You can expose other APTs you need here.
-  // ...
-})
+const ALLOWED_SET = new Set<string>(ALLOWED_IPC_CHANNELS)
+
+export const invokeAllowed = (channel: string, payload?: unknown) => {
+  if (!ALLOWED_SET.has(channel)) {
+    throw new Error(`IPC channel not allowed: ${channel}`)
+  }
+  return ipcRenderer.invoke(channel, payload)
+}
+
+export const createPraxisApi = () =>
+  Object.freeze({
+    ping: () => invokeAllowed(RUNTIME_PING),
+    getStatus: () => invokeAllowed(RUNTIME_GET_STATUS),
+    persistenceGetPaths: () => invokeAllowed(PERSISTENCE_GET_PATHS),
+    persistenceGetDbStatus: () => invokeAllowed(PERSISTENCE_GET_DB_STATUS),
+    persistenceGetDbIntegritySummary: () => invokeAllowed(PERSISTENCE_GET_DB_INTEGRITY),
+    syncGetStatus: () => invokeAllowed(SYNC_GET_STATUS),
+    backupGetInventoryPreview: () => invokeAllowed(BACKUP_GET_INVENTORY_PREVIEW),
+    restoreGetPlanPreview: (zipPath: string) =>
+      invokeAllowed(RESTORE_GET_PLAN_PREVIEW, { zipPath }),
+  })
+
+contextBridge.exposeInMainWorld('praxis', createPraxisApi())
